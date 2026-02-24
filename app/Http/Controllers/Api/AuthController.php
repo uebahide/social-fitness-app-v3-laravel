d2c3d63821f851f665e195b8153ae4803ae857a6
Hidekazu Ueba <hidekazu@example.com>
2026-02-24 12:31:20 +0100

    implement login, register, logout and me api

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+use Laravel\Sanctum\PersonalAccessToken;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class AuthController extends Controller
+{
+    /**
+     * Login.
+     *
+     * Will return cookies with `{refreshToken: string}`.
+     * @response array{data: array{user: UserResource, accessToken: string}, status: bool}
+     */
+    public function login(Request $request): JsonResponse
+    {
+        $credentials =  $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['error' => 'Wrong credentials.'], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * Register.
+     *
+     * Will return cookies with `{refreshToken: string}`.
+     * @response array{data: array{user: UserResource, accessToken: string}, status: bool}
+     */
+    public function register(Request $request): JsonResponse
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+            'user' => $user
+        ]);
+    }
+
+    public function logout(Request $request): JsonResponse
+    {
+        $token = $request->user()->currentAccessToken();
+        if ($token instanceof PersonalAccessToken) {
+            $token->delete();
+        }
+        return response()->json(['message' => 'Logged out']);
+    }
+
+    public function me(Request $request){
+        return response()->json(['user' => $request->user]);
+    }
+}
