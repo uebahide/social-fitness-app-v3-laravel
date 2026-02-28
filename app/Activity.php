883532ddfdb6e743d610173c6b858af1c491e63e
uebahide <uebahide@gmail.com>
2026-02-28 17:04:07 +0100

    Add relation between user model and activity model

--- app/Activity.php
+++ app/Activity.php
@@ -3,11 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Activity extends Model
 {
     protected $fillable = [
         'title',
         'description',
+        'user_id'
     ];
+
+    public function user(): BelongsTo{
+        return $this->belongsTo(User::class);
+    }
 }
+
+
--- app/User.php
+++ app/User.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
 
@@ -38,4 +39,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Activity::class);
+    }
 }
+
+
